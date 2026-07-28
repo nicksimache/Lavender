20ccfbd9ee96e888005978311d8497ba2436eb22
nicksimache <nicksimache@gmail.com>
2026-07-28 00:26:03 -0700

    added icon

--- obj/Debug/net8.0-windows/Lavender.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Lavender.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lavender")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6485d33642ac01ae77736d9d3582e21aa57435ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ee68853b0260147236f2b86e127103571c0620")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lavender")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lavender")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
